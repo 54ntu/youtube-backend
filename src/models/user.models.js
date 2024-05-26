2e29e703113a10ef9a48d04e27c848bb55bd8c5c
54ntu <chaudharysantaram8@gmail.com>
2024-05-26 17:42:30 +0545

    cloudinary settings and utils has been created

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -56,7 +56,7 @@ userSchema.methods.generateAccessToken = function () {
   return jwt.sign(
     {
       _id: this._id,
-      email:this.email
+      email: this.email,
     },
 
     process.env.ACCESS_TOKEN_SECRET,
