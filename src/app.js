82015cdbc217915b4fca8030ff164a4324d9314a
54ntu <chaudharysantaram8@gmail.com>
2024-05-27 00:12:07 +0545

    register controller, user routes && bug fixed at asyncHandler utils

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+const { router } = require('./routes/user.routes');
 const app = express()
 
 
@@ -11,6 +12,7 @@ app.use(express.urlencoded({extended:true})); // to read the data from url
 app.use(express.static('public')) // this middleware helps to handle the image file
 app.use(cookieParser()); // we can access the cookies data from the browser
 
-
+//main route to the router
+app.use("/api/v1",router);
 
 module.exports={app};
