eba740a41d4ca5e8048395bdbb14f6129149f069
54ntu <chaudharysantaram8@gmail.com>
2024-06-04 02:48:33 +0545

    playlist controllers are created

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,39 +1,96 @@
+const { isValidObjectId } = require("mongoose");
 const { Playlist } = require("../models/playlists.models");
-const {ApiError} = require("../utils/ApiError");
+const { ApiError } = require("../utils/ApiError");
 const { ApiResponse } = require("../utils/ApiResponse");
-const {asyncHandler} = require("../utils/asyncHandler")
+const { asyncHandler } = require("../utils/asyncHandler");
+const { Video } = require("../models/video.models");
 
 const createPlaylist = asyncHandler(async (req, res) => {
   //TODO: create playlist
   // get name and description from req.body
   //check if empty or not
-  
+
   const { name, description } = req.body;
-  if(!name && ! description){
-    throw new ApiError(400," name and descriptions are required")
+  if (!name && !description) {
+    throw new ApiError(400, " name and descriptions are required");
   }
 
-  const ownerId = req.user?._id
+  const ownerId = req.user?._id;
   const playlist = await Playlist.create({
     name,
     description,
-    owner:ownerId
-  })
-
+    owner: ownerId,
+  });
 
   // console.log(playlist);
-  if(!playlist){
-    throw new ApiError(500,"error while creating playlist")
+  if (!playlist) {
+    throw new ApiError(500, "error while creating playlist");
   }
 
-  return res.status(200)
-  .json(
-    new ApiResponse(200, playlist, "playlist created successfully")
-  )
+  return res
+    .status(200)
+    .json(new ApiResponse(200, playlist, "playlist created successfully"));
+});
 
+const getUserPlaylists = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+  //TODO: get user playlists
+  // console.log(userId);
 
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "invalid user id");
+  }
+
+  const userPlaylist = await Playlist.aggregate([
+    {
+      $match: {
+        owner: userId,
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "videos",
+        foreignField: "_id",
+        as: "videoDetails",
+      },
+    },
+  ]);
+
+  // console.log(userPlaylist)
+
+ 
 });
 
 
+ const addVideoToPlaylist = asyncHandler(async (req, res) => {
+   const { playlistId, videoId } = req.params;
+   if(!isValidObjectId(playlistId)){
+    throw new ApiError(400,"invalid playlist id")
+   }
 
-module.exports = { createPlaylist };
+   if(!isValidObjectId(videoId)){
+    throw new ApiError(400,"invalid video id")
+   }
+   
+   const video = await Video.findById(videoId);
+  //  console.log(video)
+   
+
+   const playlist = await Playlist.findByIdAndUpdate(
+    playlistId,
+   {
+    $set:{
+      videos:videoId
+    }
+   },
+   {
+    new:true
+   }
+   )
+   console.log(playlist);
+
+
+ });
+
+module.exports = { createPlaylist, getUserPlaylists, addVideoToPlaylist };
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const {verifyJWT} = require("../middlewares/auth.middlewares")
-const { createPlaylist } = require("../controllers/playlist.controller")
+const { createPlaylist, getUserPlaylists, addVideoToPlaylist } = require("../controllers/playlist.controller")
 const playlistRouter = express.Router()
 
 playlistRouter.route("/createPlaylist").post(verifyJWT,createPlaylist)
+playlistRouter.route("/getUserPlaylist/:userId").get(verifyJWT, getUserPlaylists);
+playlistRouter.route("/add/:videoId/:playlistId").patch(verifyJWT,addVideoToPlaylist);
 
 
 
