dcd124c0e8296e0165dcffb0fa2f14b15c824ce3
54ntu <chaudharysantaram8@gmail.com>
2024-06-06 16:29:16 +0545

    getchannelstats controller is created

--- src/controllers/dashboard.coontrollers.js
+++ src/controllers/dashboard.coontrollers.js
@@ -1,11 +1,80 @@
 const { asyncHandler } = require("../utils/asyncHandler");
 const { Video } = require("../models/video.models");
+const {Subscription} = require("../models/subcriptions.models")
 const { default: mongoose, isValidObjectId } = require("mongoose");
 const { ApiError } = require("../utils/ApiError");
-const {ApiResponse} = require("../utils/ApiResponse")
+const { ApiResponse } = require("../utils/ApiResponse");
+const { User } = require("../models/user.models");
 
 const getChannelStats = asyncHandler(async (req, res) => {
   // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+  const userId = req.user?._id;
+  //  console.log(userId)
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, " invalid channel id");
+  }
+
+  const videos = await Video.aggregate([
+    {
+        $match:{
+            owner :new mongoose.Types.ObjectId(userId)
+        }
+    },
+    {
+        $lookup:{
+            from :"likes",
+            localField:"_id",
+            foreignField:"video",
+            as:"likes"
+        }
+    },
+    {
+        $group:{
+            _id:null,
+            totalViews:{$sum:"$views"},
+            totalLikes :{$sum:{$size:"$likes"}},
+            totalvideos:{$sum:1}
+        }
+    }
+
+
+  ])
+
+
+  const subscribercount = await Subscription.aggregate([
+    {
+        $match:{
+            channel:new mongoose.Types.ObjectId(userId)
+        }
+    },
+    {
+        $addFields:{
+            totalsubscribers:{
+                $sum:1
+            }
+        }
+    }
+
+  ])
+
+
+  const channelStats={
+    totalvideos:videos[0]?.totalvideos || 0,
+    totalLikes: videos[0]?.totalLikes || 0,
+    totalViews: videos[0]?.totalViews || 0,
+    totalsubscribers : subscribercount[0]?.totalsubscribers || 0
+  }
+
+
+  if(!channelStats){
+    throw new ApiError(500,"error on fetching channel stats")
+  }
+
+  return res.status(200)
+  .json(new ApiResponse(
+    200, channelStats," channel status fetched successfully"
+  ))
+
 });
 
 const getChannelVideos = asyncHandler(async (req, res) => {
@@ -50,13 +119,13 @@ const getChannelVideos = asyncHandler(async (req, res) => {
     },
   ]);
 
-  if(!channelVideos){
-    throw new ApiError(500,"error fetching the videos")
+  if (!channelVideos) {
+    throw new ApiError(500, "error fetching the videos");
   }
 
-  return res.status(200)
-  .json(new ApiResponse(200, channelVideos,"videos fetched successfully!!"))
-
+  return res
+    .status(200)
+    .json(new ApiResponse(200, channelVideos, "videos fetched successfully!!"));
 });
 
 module.exports = {
